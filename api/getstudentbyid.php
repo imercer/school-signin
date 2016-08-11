21924054c4769659c4dae98908b01bea2306ba69
Isaac Mercer <Isaac Mercer>
2016-08-11 18:37:28 +1200

    Add database and justification screen

---
+++ api/getstudentbyid.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+$idnumber = $_GET['idnumber'];
+$servername = "localhost";
+$username = "school-signin";
+$password = "uJSZPJRZF8EfG6WX";
+$dbname = "gdc-signin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM `student-names` WHERE studentid=$idnumber";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["studentid"]. " - Name: " . $row["firstname"]. " " . $row["familyname"]. "<br>";
+		$_SESSION['studentid'] = $row["studentid"];
+		$_SESSION['firstname'] = $row["firstname"];
+		$_SESSION['familyname'] = $row["familyname"];
+		$_SESSION['form'] = $row["formclass"];
+	    echo "<script>window.location.href='../justification.php'</script>";
+    }
+} else {
+    echo "<script>window.location.href='../index.html'</script>";
+}
+$conn->close();
+
+?>
---
+++ justification.php
@@ -0,0 +1,96 @@
+<?php session_start();?>
+<!doctype html>
+<!-- Copyright Isaac Mercer 2016
+-->
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="css/material.min.css">
+
+<script defer src="js/material.min.js"></script>
+<meta charset="utf-8">
+<title>School Signin</title>
+</head>
+<body>
+<!-- Always shows a header, even in smaller screens. -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">Glendowie College Sign In</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+      <div id="time"></div>
+      <!-- Navigation. We hide it in small screens. -->
+    </div>
+  </header>
+  <main class="mdl-layout__content">
+    <div class="page-content" > 
+          <h3 style="text-align:center">Good Morning <?php echo $_SESSION['firstname'] . ' ' . $_SESSION['familyname'];?>!, Please enter a short justification for your late arrival.</h3>
+
+   	  <div style="width: 20%;margin:auto;">
+            <form action="api/process.php" method="get" id="initialform">
+              <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <textarea class="mdl-textfield__input" type="text" rows= "3" id="reasonfield" ></textarea>
+			    <label class="mdl-textfield__label" for="reasonfield">Justification.</label>
+              </div>
+            </form>
+                       
+        </div>
+        <div class="mdl-grid" style="text-align:center">
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'q'; document.getElementById('reasonfield').focus()">Q</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'w'; document.getElementById('reasonfield').focus()">W</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'e'; document.getElementById('reasonfield').focus()">E</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'r'; document.getElementById('reasonfield').focus()">R</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 't'; document.getElementById('reasonfield').focus()">T</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'y'; document.getElementById('reasonfield').focus()">Y</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'u'; document.getElementById('reasonfield').focus()">U</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'i'; document.getElementById('reasonfield').focus()">I</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'o'; document.getElementById('reasonfield').focus()">O</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'p'; document.getElementById('reasonfield').focus()">P</button></div>
+    <div class="mdl-cell mdl-cell--1-col"></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value.slice(0, -1); document.getElementById('reasonfield').focus()"><i class="material-icons">backspace</i></button></div>
+</div>
+	</div>
+            <div class="mdl-grid" style="text-align:center">
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'a'; document.getElementById('reasonfield').focus()">A</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 's'; document.getElementById('reasonfield').focus()">S</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'd'; document.getElementById('reasonfield').focus()">D</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'f'; document.getElementById('reasonfield').focus()">F</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'g'; document.getElementById('reasonfield').focus()">G</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'h'; document.getElementById('reasonfield').focus()">H</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'j'; document.getElementById('reasonfield').focus()">J</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'k'; document.getElementById('reasonfield').focus()">K</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'l'; document.getElementById('reasonfield').focus()">L</button></div>
+  <div class="mdl-cell mdl-cell--1-col"></div>  <div class="mdl-cell mdl-cell--1-col"></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('initialform').submit();
+"><i class="material-icons">keyboard_return</i></button></div>
+</div>
+ <div class="mdl-grid" style="text-align:center">
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'z'; document.getElementById('reasonfield').focus()">Z</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'x'; document.getElementById('reasonfield').focus()">X</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'c'; document.getElementById('reasonfield').focus()">C</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'v'; document.getElementById('reasonfield').focus()">V</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'b'; document.getElementById('reasonfield').focus()">B</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'n'; document.getElementById('reasonfield').focus()">N</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + 'm'; document.getElementById('reasonfield').focus()">M</button></div>
+  <div class="mdl-cell mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + '.'; document.getElementById('reasonfield').focus()">.</button></div>
+  <div class="mdl-cell mdl-cell--1-col"></div>  <div class="mdl-cell mdl-cell--1-col"></div>
+</div>
+ <div class="mdl-grid" style="text-align:center">
+  <div class="mdl-cell mdl-cell--1-col"></div><div class="mdl-cell mdl-cell--1-col"></div><div class="mdl-cell mdl-cell--1-col"></div><div class="mdl-cell mdl-cell--5-col"><button style="width: 100%" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="document.getElementById('reasonfield').value = document.getElementById('reasonfield').value + ' '; document.getElementById('reasonfield').focus()">          </button></div>
+</div>
+	</div>
+  </main>
+</div>
+<script type="application/javascript">
+setInterval(function(){
+  	var date = new Date();
+	var n = date.toDateString();
+	var time = date.toLocaleTimeString();
+	
+	document.getElementById('time').innerHTML = n + ' ' + time;
+}, 1000);
+</script>
+</body>
+</html>
